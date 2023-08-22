c8d7dde3603e15b51959db1961fc9d4416cebb28
アカメ <141885614+AkameBYTES@users.noreply.github.com>
2023-08-21 21:44:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+//Exercicío 1
+/*a. false
+b. false
+c.true
+d. boolean
+
+//Exercicío 2
+meu querido colega colocou "let" em vez de "const", aí o resultado fica errado
+
+/*Exercicío 3
+const primeiroNumero = prompt("Digite um número")
+const segundoNumero = prompt("Digite outro número")
+const soma = primeroNumero + segundoNumero
+*/
+
+//Exercicío 5
+/*const idadeEu = prompt("Qual a sua idade?")
+const idadeBest = prompt("Qual a idade do seu abiguinho?")
+const comparacao = idadeEu > idadeBest
+console.log(comparacao)
+Deu true, pois eu tenho 15 e meu abigo tem 14*/
+
+//Exercicío 6
+/*const numeroPar = prompt("Escolha um número PAR")
+const soma = numeroPar % 2
+
+console.log(soma)*/
+//todos dão 0, pois 2 dividido por pares,mais os restos, eventualmente dará 0
+
+/*const numeroImpar = prompt("Escolha um número ímpar")
+const soma = numeroImpar % 2
+console.log(soma)*/
+//agora deu 1, porque par com ímpar sobra 1
+
+//Exercicío 7
+/*const idadeUsuario = prompt("Qual a sua idade")
+const idadeMeses = Number(idadeUsuario) * 12
+const idadeDias = Number(idadeMeses) * 30
+const idadeHoras = Number(idadeDias) * 24
+console.log(idadeMeses)
+console.log(idadeDias)
+console.log(idadeHoras)
+*/
+
+
+
+
